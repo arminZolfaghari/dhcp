c6fd4f883c42e3e55806fa5f59c3baa4e3fa826c
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-15 22:25:28 +0430

    add request message in client side

--- Client.py
+++ Client.py
@@ -1,17 +1,25 @@
 import socket
 import dhcppython
 
+MAC_ADDRESS = "de:ad:be:ef:c0:de"
+
 
 # create UDP socket for client
 def create_udp_socket():
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # enable broadcasting mode
     return client
 
 
 # create discovery message with mac_address by dhcppython library
 def create_discovery_message(mac_address):
     return dhcppython.packet.DHCPPacket.Discover(mac_address)
+
+
+# create request message with mac_address by dhcppython library
+def create_request_message(mac_address):
+    return dhcppython.packet.DHCPPacket.Request(mac_address)
 
 
 message = b"give me IP!!!"
@@ -27,5 +35,8 @@ if __name__ == "__main__":
 
     took_ip_from_dhcp = False
     while not took_ip_from_dhcp:
-        di
-
+        # create discovery message and send to all DHCP server (broadcast)
+        discovery_msg = create_discovery_message(MAC_ADDRESS)
+        client_socket.sendto(discovery_msg.asbytes, ('<broadcast>', 30067))
+        response_from_DHCP_server, address = client_socket.recvfrom(1024)
+        print("received message from DHCP server : ", response_from_DHCP_server)
--- DHCPServer.py
+++ DHCPServer.py
@@ -1,12 +1,32 @@
-import socket
+import socket, threading, subprocess
+import dhcppython
 import time
 
 
 # create UDP socket for DHCP server
 def create_udp_socket():
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) #UDP
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # enable broadcasting mode
     return server
+
+
+def create_offer_message():
+    pass
+
+
+# DHCP server must be multi thread
+def start_DHCP_server(server):
+    server.listen()
+    while True:
+        connection, address = server.accept()
+        print("connection is {} and address is {}".format(connection, address))
+        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread.start()
+
+
+def handle_client(connection, address):
+    pass
 
 
 # message = b"this is your IP"
@@ -17,5 +37,10 @@ def create_udp_socket():
 
 if __name__ == "__main__":
     DHCP_server = create_udp_socket()
-    DHCP_server.bind(("", 30067))   # 30067 for DHCP server port
+    DHCP_server.bind(("", 30067))  # 30067 for DHCP server port
     # DHCP_server.settimeout(0.2)
+
+    print(DHCP_server)
+
+    print("*** DHCP server is starting ***")
+    start_DHCP_server(DHCP_server)
