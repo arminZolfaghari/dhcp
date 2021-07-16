eef1704de55aaa263f9e1bf07b0eba2564fb83d3
Armin Zolfaghari Daryani <arminzd1@gmail.com>
2021-07-16 19:29:36 +0430

    complete DHCP discovery, offer, request, ack and nack in client and DHCP server

--- Client.py
+++ Client.py
@@ -1,5 +1,5 @@
 import socket
-import dhcppython
+import dhcppython, ipaddress
 
 MAC_ADDRESS = "de:ad:be:ef:c0:de"
 
@@ -7,7 +7,7 @@ MAC_ADDRESS = "de:ad:be:ef:c0:de"
 # create UDP socket for client
 def create_udp_socket():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # enable broadcasting mode
     return client
 
@@ -18,19 +18,44 @@ def create_discovery_message(mac_address):
 
 
 # create request message with mac_address by dhcppython library
-def create_request_message(mac_address):
-    return dhcppython.packet.DHCPPacket.Request(mac_address)
-
+def create_request_message(mac_address, xid, offer_ip):
+    # return dhcppython.packet.DHCPPacket.Request(mac_address, seconds=0, tx_id=xid, yiaddr=ipaddress.IPv4Address("192.168.1.6"))
+    return dhcppython.packet.DHCPPacket(op="BOOTREQUEST", htype="ETHERNET", hlen=6, hops=0, xid=xid, secs=0, flags=0, ciaddr=ipaddress.IPv4Address(0), yiaddr=ipaddress.IPv4Address(offer_ip), siaddr=ipaddress.IPv4Address(0), giaddr=ipaddress.IPv4Address(0), chaddr=mac_address, sname=b'', file=b'', options=dhcppython.options.OptionList([dhcppython.options.options.short_value_to_object(53, "DHCPREQUEST")]))
 
 
 if __name__ == "__main__":
     client_socket = create_udp_socket()
-    client_socket.bind(('', 30068))  # 30068 for client port
+    client_socket.bind(('', 6668))  # 30068 for client port
 
     took_ip_from_dhcp = False
-    while not took_ip_from_dhcp:
-        # create discovery message and send to all DHCP server (broadcast)
-        discovery_msg = create_discovery_message(MAC_ADDRESS)
-        client_socket.sendto(discovery_msg.asbytes, ('<broadcast>', 30067))
+    # while not took_ip_from_dhcp:
+    # create discovery message and send to all DHCP server (broadcast)
+    discovery_msg = create_discovery_message(MAC_ADDRESS)
+    client_socket.sendto(discovery_msg.asbytes, ('localhost', 6667))
+    print("Client send discovery message.(broadcast)")
+    response_from_DHCP_server, address = client_socket.recvfrom(1024)
+    dhcp_packet = dhcppython.packet.DHCPPacket.from_bytes(response_from_DHCP_server)
+    dhcp_message_type = dhcp_packet.options.as_dict()["dhcp_message_type"]
+    if dhcp_message_type == "DHCPOFFER":
+        offer_ip = dhcp_packet.yiaddr
+        dhcp_message_xid = dhcp_packet.xid
+        print("Client get DHCP offer message from DHCP server, offer ip: {}".format(offer_ip))
+        request_msg = create_request_message(MAC_ADDRESS, dhcp_message_xid, offer_ip)
+        client_socket.sendto(request_msg.asbytes, ('localhost', 6667))
+        print("Client send request message to get this ip {} from DHCP server".format(offer_ip))
+
+    while True:
         response_from_DHCP_server, address = client_socket.recvfrom(1024)
-        print("received message from DHCP server : ", response_from_DHCP_server)
+        dhcp_packet = dhcppython.packet.DHCPPacket.from_bytes(response_from_DHCP_server)
+        dhcp_message_type = dhcp_packet.options.as_dict()["dhcp_message_type"]
+        if dhcp_message_type == "DHCPACK":
+            client_ip = dhcp_packet.yiaddr
+            print("Client get ACK message from DHCP server for this ip : {}".format(client_ip))
+            break
+
+        elif dhcp_message_type == "DHCPNACK":
+            print("Client get NACK message from DHCP server!!!")
+            break
+
+
+
--- DHCPServer.py
+++ DHCPServer.py
@@ -1,11 +1,13 @@
+import random
 import socket, threading, subprocess
-import dhcppython
+import dhcppython, ipaddress
 import json
 import time
 
-
 MACADDRESS_IP_DICT = {}
 IP_POOL = []
+MAC_ADDRESS = "de:a0:be:ef:c0:de"
+
 
 
 # read config.json
@@ -20,10 +22,9 @@ def create_ip_pool_range(from_ip, to_ip):
     ip_pool_arr = []
     from_ip_parts = from_ip.split('.')
     to_ip_parts = to_ip.split('.')
-    print(from_ip_parts)
     for i in range(int(from_ip_parts[3]), int(to_ip_parts[3]) + 1):
         ip = from_ip_parts[0] + "." + from_ip_parts[1] + "." + from_ip_parts[2] + "." + str(i)
-        ip_pool_arr.append(ip)
+        ip_pool_arr.append(ipaddress.IPv4Address(ip))
 
     return ip_pool_arr
 
@@ -76,43 +77,101 @@ def extract_server_config():
 # create UDP socket for DHCP server
 def create_udp_socket():
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # enable broadcasting mode
     return server
 
 
-def create_offer_message(mac_address):
-    return
+# select ip from pool and give offer to client
+def select_ip_from_pool():
+    if len(IP_POOL) > 0:
+        selected_ip = random.choice(IP_POOL)
+        # x.pop(random.randrange(len(x)))
+        return selected_ip
 
 
-def handle_client(data, client_address):
+def create_offer_message(mac_address, xid):
+    offer_ip = select_ip_from_pool()
+    print("DHCP Server get offer ip to client : ", offer_ip)
+    return dhcppython.packet.DHCPPacket.Offer(mac_address, seconds=0, tx_id=xid, yiaddr=ipaddress.IPv4Address(offer_ip))
+
+
+def create_ack_message(mac_address, xid, allocated_ip):
+    return dhcppython.packet.DHCPPacket(op="BOOTREPLY", htype="ETHERNET", hlen=6, hops=0, xid=xid, secs=0, flags=0,
+                                 ciaddr=ipaddress.IPv4Address(0), yiaddr=ipaddress.IPv4Address(allocated_ip),
+                                 siaddr=ipaddress.IPv4Address(0), giaddr=ipaddress.IPv4Address(0),
+                                 chaddr=mac_address, sname=b'', file=b'', options=dhcppython.options.OptionList(
+            [dhcppython.options.options.short_value_to_object(53, "DHCPACK")]))
+
+
+def create_nack_message(mac_address, xid):
+    return dhcppython.packet.DHCPPacket(op="BOOTREPLY", htype="ETHERNET", hlen=6, hops=0, xid=xid, secs=0, flags=0,
+                                        ciaddr=ipaddress.IPv4Address(0), yiaddr=ipaddress.IPv4Address(0),
+                                        siaddr=ipaddress.IPv4Address(0), giaddr=ipaddress.IPv4Address(0),
+                                        chaddr=mac_address, sname=b'', file=b'', options=dhcppython.options.OptionList(
+            [dhcppython.options.options.short_value_to_object(53, "DHCPNACK")]))
+
+
+def check_client_ip_request(client_mac_address, requested_ip):
+    global IP_POOL, MACADDRESS_IP_DICT
+    if requested_ip in IP_POOL:
+        IP_POOL.remove(requested_ip)
+        MACADDRESS_IP_DICT[client_mac_address] = requested_ip
+        return True
+
+    elif requested_ip not in IP_POOL and client_mac_address not in MACADDRESS_IP_DICT:
+        return False
+    else:
+        return True
+
+
+def handle_client(server, data, client_address, socket_lock):
     dhcp_packet = dhcppython.packet.DHCPPacket.from_bytes(data)
     dhcp_message_type = dhcp_packet.options.as_dict()["dhcp_message_type"]
+    dhcp_message_xid = dhcp_packet.xid
     if dhcp_message_type == "DHCPDISCOVER":
         print("DHCP Server get discover message from client")
+        # print("ip pool len", len(IP_POOL))
+        offer_msg = create_offer_message(MAC_ADDRESS, dhcp_message_xid)
+        server.sendto(offer_msg.asbytes, ('<broadcast>', 6668))
 
-    print()
-
+    elif dhcp_message_type == "DHCPREQUEST":
+        requested_ip = dhcp_packet.yiaddr
+        print("DHCP Server get request message from client.(requested ip : {})".format(requested_ip))
+        allocate_ip_flag = check_client_ip_request(dhcp_packet.chaddr, requested_ip)
+        if allocate_ip_flag:
+            ack_msg = create_ack_message(MAC_ADDRESS, dhcp_message_xid, requested_ip)
+            print("DHCP Server send ACK to client.(ip {})".format(requested_ip))
+            server.sendto(ack_msg.asbytes, ('<broadcast>', 6668))
+        else:
+            nack_msg = create_nack_message(MAC_ADDRESS, dhcp_message_xid)
+            print("DHCP Server send NACK to client.(ip {})".format(requested_ip))
+            server.sendto(nack_msg.asbytes, ('<broadcast>', 6668))
 
 
 # wait for clients and handle their requests
-def wait_for_clients(server):
+def wait_for_clients(server, socket_lock):
     while True:
-        try:  # receive request from client
-            data_from_client, client_address = server.recvfrom(1024)
-            print("connection is {} and address is {}".format(data_from_client, client_address))
-            # dhcp_packet = dhcppython.packet.DHCPPacket.from_bytes(data_from_client)
-            # # print(dhcp_packet)
-            thread = threading.Thread(target=handle_client, args=(data_from_client, client_address))
-            thread.start()
-
-        except OSError as err:
-            print(err)
+        # receive request from client
+        data_from_client, client_address = server.recvfrom(10000)
+        # dhcp_packet = dhcppython.packet.DHCPPacket.from_bytes(data_from_client)
+        # # print(dhcp_packet)
+        handle_client(server, data_from_client, client_address, socket_lock)
+        # if data_from_client:
+        #     thread = threading.Thread(target=handle_client,
+        #                               args=(server, data_from_client, client_address, socket_lock))
+        #     thread.setDaemon(True)
+        #     thread.start()
 
 
 # DHCP server must be multi thread
 def start_DHCP_server(server):
-    wait_for_clients(server)
+    ip_pool_arr, lease_time, reservation_list, black_list = extract_server_config()
+    global IP_POOL
+    IP_POOL = ip_pool_arr
+    # socket_lock = threading.Lock()
+    socket_lock = 2
+    wait_for_clients(server, socket_lock)
     # server.listen()
     # while True:
     #     data_from_client, address = server.recvfrom(1024)
@@ -132,7 +191,7 @@ def start_DHCP_server(server):
 
 if __name__ == "__main__":
     DHCP_server = create_udp_socket()
-    DHCP_server.bind(('192.168.1.33', 30067))  # 30067 for DHCP server port
+    DHCP_server.bind(('localhost', 6667))  # 30067 for DHCP server port
     # DHCP_server.settimeout(0.2)
 
     print(DHCP_server)
